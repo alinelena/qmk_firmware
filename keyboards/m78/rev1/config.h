bc1759c828c77978d4d5f13045304d53d43e5121
Alin M Elena <alin-marin.elena@stfc.ac.uk>
2020-11-08 22:41:14 +0000

    it shall compile now

--- keyboards/m78/rev1/config.h
+++ keyboards/m78/rev1/config.h
@@ -10,6 +10,7 @@
 #define MATRIX_COLS 13
 
 /* key matrix pins */
+//                         0   1   2   3   4   5
 #define MATRIX_ROW_PINS { B0, B1, B2, B3, B7, D0 }
 #define MATRIX_COL_PINS { D7, B4, B5, B6, F7, F6, F5, F4, C7, C6, D3, D2, D1 }
 //                         0   1   2   3   4   5   6   7   8    9  10  11  12
--- keyboards/m78/rev2/config.h
+++ keyboards/m78/rev2/config.h
@@ -5,22 +5,12 @@
 
 #undef MATRIX_ROWS
 #undef MATRIX_COLS
+
 /* key matrix size */
 #define MATRIX_ROWS 6
 #define MATRIX_COLS 13
 
 /*
-  [_QW] = KEYMAP(
-         0                1               2       3         4      5      6        7       8       9       10             11           12
-       KC_ESC,           KC_F1,        KC_F2,   KC_F3,    KC_F4, KC_F5,  KC_F6,   KC_F7,  KC_F8,   KC_F9,  KC_F10,      KC_F11,      KC_F12,
-       KC_GRV,            KC_1,         KC_2,    KC_3,     KC_4,  KC_5,   KC_6,    KC_7,   KC_8,    KC_9,    KC_0,     KC_MINS,      KC_EQL,
-       KC_TAB,            KC_Q,         KC_W,    KC_E,     KC_R,  KC_T,   KC_Y,    KC_U,   KC_I,    KC_O,    KC_P, KC_LBRACKET, KC_RBRACKET,
-      KC_SLSH,            KC_A,         KC_S,    KC_D,     KC_F,  KC_G,   KC_H,    KC_J,   KC_K,    KC_L, KC_SCLN,     KC_QUOT,      KC_ENT,
-      KC_LSFT, KC_NONUS_BSLASH,         KC_Z,    KC_X,     KC_C,  KC_V,   KC_B,    KC_N,   KC_M, KC_COMM,  KC_DOT,       KC_UP,     KC_NUHS,
-      KC_LCTL,         KC_LGUI, TT(_Fortran), KC_LALT, TT(_CPP),KC_SPC, KC_SPC, KC_RALT, KC_DEL, KC_BSPC, KC_LEFT,     KC_DOWN,      KC_RGHT),
-*/
-   /*
-
 3V3 GNG  5V  B9  B8  B7  B6  B5  B4  B3 A15 A12 A11 A10  A9  A8 B15 B14 B13 B12
                                                                                USB connector
 VBT C13 C14 C15  A0  A1  A2  A3  A4  A5  A6  A7  B0  B1 B10 B11 RST 3V3 GND GND
@@ -30,16 +20,13 @@ B7 LED
 A1 avoid
 A4 avoid
 A6 avoid
+A11 avoid usb
+A12 avoid usb
 */
 
 //                          0    1    2    3   4   5
-#define MATRIX_ROW_PINS { B12, B13, B14, B15, A8, A9}
+#define MATRIX_ROW_PINS { B12, B13, B14, B15, A8, A9 }
 //                         0   1   2   3   4   5    6    7   8   9  10   11   12
-#define MATRIX_COL_PINS { A1, A2, A3, A7, B0, B1, B10, B11, B5, B4, B3, A15, A10}
-#define ENCODERS_PAD_A {  }
-#define ENCODERS_PAD_B {  }
-#define MATRIX_ROW_PIN { }
+#define MATRIX_COL_PINS { A0, A2, A3, A7, B0, B1, B10, B11, B5, B4, B3, A15, A10 }
 
-#define DIP_SWITCH_PINS { }
-#endif
-
+#endif // CONFIG_REV2_H
