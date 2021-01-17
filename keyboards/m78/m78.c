1f4a688edc68e96f61100b6d0e9a39db827cbe7b
Alin M Elena <alin-marin.elena@stfc.ac.uk>
2021-01-17 11:57:56 +0000

    f103

--- keyboards/m78/m78.c
+++ keyboards/m78/m78.c
@@ -1,1 +1,18 @@
 #include "m78.h"
+
+
+
+#ifdef KEYBOARD_m78_rev3
+void keyboard_pre_init_kb(void){
+    // Workaround for reversible pcb/mcu
+    palSetLineMode(C13, PAL_MODE_OUTPUT_OPENDRAIN);
+    palSetLineMode(B9, PAL_MODE_OUTPUT_OPENDRAIN);
+    palSetLineMode(B8, PAL_MODE_OUTPUT_OPENDRAIN);
+
+    keyboard_pre_init_user();
+}
+
+void board_init(void) {
+    AFIO->MAPR |= AFIO_MAPR_USART1_REMAP | AFIO_MAPR_TIM3_REMAP_PARTIALREMAP;
+}
+#endif
--- keyboards/m78/rev3/config.h
+++ keyboards/m78/rev3/config.h
@@ -1,9 +1,9 @@
-
-
 #ifndef CONFIG_REV3_H
 #define CONFIG_REV3_H
 
-#define DEVICE_VER      0x0002
+#define DEVICE_VER      0x0003
+#undef MANUFACTURER
+#define MANUFACTURER    alinb
 
 #undef MATRIX_ROWS
 #undef MATRIX_COLS
@@ -12,18 +12,33 @@
 #define MATRIX_ROWS 6
 #define MATRIX_COLS 13
 
+/*
+3V3 GNG  5V  B9  B8  B7  B6  B5  B4  B3 A15 A12 A11 A10  A9  A8 B15 B14 B13 B12
+                                                                               USB connector
+VBT C13 C14 C15  A0  A1  A2  A3  A4  A5  A6  A7  B0  B1 B10 B11 RST 3V3 GND GND
 
-#define DIODE_DIRECTION COL2ROW
+B6 LED
+B7 LED
+A1 avoid
+A4 avoid
+A6 avoid
+A11 avoid usb
+A12 avoid usb
+*/
+
 //                          0    1    2    3   4   5
+//wired
+//#define MATRIX_ROW_PINS { B12, B13, B14, B15, A8, A9 }
+
 //pcb
 #define MATRIX_ROW_PINS { B11, B1, B0,  A7, A2, A0 }
 
 
 //                         0   1   2   3   4   5    6    7   8   9  10   11   12
-#define MATRIX_COL_PINS { A9, A10, A15, B3, B4, B5, B6, B7, B8, B9, C15, A3, B10 }
 //wired
+//#define MATRIX_COL_PINS { A0, A2, A3, A7, B0, B1, B10, B11, B5, B4, B3, A15, A10 }
 
-#define DEBOUNCE    5
-#define LOCKING_SUPPORT_ENABLE
-#define LOCKING_RESYNC_ENABLE
-#endif
+//pcb
+#define MATRIX_COL_PINS { A9, A10, A15, B3, B4, B5, B6, B7, B8, B9, C15, A3, B10 }
+//
+#endif // CONFIG_REV2_H
