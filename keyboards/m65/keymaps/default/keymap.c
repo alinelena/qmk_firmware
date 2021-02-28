f7ee6a596265f39e47bcf091b80e0a5244bb8e4e
Alin Marin Elena <alin@elena.space>
2021-02-28 08:20:32 +0000

    Update keyboards/m65/keymaps/default/keymap.c
    Co-authored-by: Drashna Jaelre <drashna@live.com>

--- keyboards/m65/keymaps/default/keymap.c
+++ keyboards/m65/keymaps/default/keymap.c
@@ -15,7 +15,7 @@ You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
-#include "m65.h"
+#include QMK_KEYBOARD_H
 
 #define TAP(keycode) tap_code16(keycode)
 
