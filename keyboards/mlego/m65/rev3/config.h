42fd255bc8760ee37bab1a6ab010ed06bedbd843
alin elena <alin@elena.space>
2023-02-26 13:53:48 +0000

    [mlego] m65 various revisions and few conversions

--- keyboards/mlego/m65/rev3/config.h
+++ keyboards/mlego/m65/rev3/config.h
@@ -1,7 +1,10 @@
-// Copyright 2021-2023 alin m elena (@alinelena)
+// Copyright 2020-2023 alin m elena (@alinelena, @drFaustroll)
 // SPDX-License-Identifier: GPL-2.0-or-later
-
 #pragma once
 
 #define LED_RSE_PIN B13
 #define LED_LWR_PIN B12
+
+#if defined(CONSOLE_ENABLE)
+#define DEBUG_MATRIX_SCAN_RATE
+#endif
