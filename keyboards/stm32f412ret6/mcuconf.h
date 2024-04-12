825cbfae89ae77dbeb029126c44a9af78866ac99
alin m elena <alinm.elena@gmail.com>
2024-04-12 10:28:58 +0100

    fixes for weact412

--- keyboards/stm32f412ret6/mcuconf.h
+++ keyboards/stm32f412ret6/mcuconf.h
@@ -1,7 +1,7 @@
 #pragma once
 
 #include_next <mcuconf.h>
-
+/*
 #undef STM32_PLLM_VALUE
 #define STM32_PLLM_VALUE 4
 
@@ -10,3 +10,4 @@
 
 #undef STM32_PLLSAIM_VALUE
 #define STM32_PLLSAIM_VALUE 4
+*/
