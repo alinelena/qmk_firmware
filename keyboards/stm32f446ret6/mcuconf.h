ff98c0e6e91a4573a45dbd52a8f0795710dd0590
alin elena <alin@elena.re>
2024-04-12 10:23:15 +0100

    add weact 412/405/446

---
+++ keyboards/stm32f446ret6/mcuconf.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#include_next <mcuconf.h>
+
+#undef STM32_PLLM_VALUE
+#define STM32_PLLM_VALUE 4
+
+#undef STM32_PLLI2SM_VALUE
+#define STM32_PLLI2SM_VALUE 4
+
+#undef STM32_PLLSAIM_VALUE
+#define STM32_PLLSAIM_VALUE 4
